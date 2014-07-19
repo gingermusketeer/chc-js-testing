2f1f1e7b75e52c582b82b108b2274a90874059fa
Max Brosnahan <maximilianbrosnahan@gmail.com>
2014-07-19 20:02:57 +1200

    Add better tape reporter when in a browser

--- tape/spec/test.js
+++ tape/spec/test.js
@@ -1,3 +1,13 @@
+// Hackery below allows this to be run in both node and the browser
+// This is not an issue with tape but rather an issue with tap-browser-color
+try {
+  GLOBAL;
+} catch (e){
+  // Browser
+  require('tap-browser-color')();
+}
+
+
 var test = require('tape');
 
 test('timing test', function (t) {
